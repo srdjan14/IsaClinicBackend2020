8f939daf1b285290aca4ec5eee4dc951b668f13f
srdjan14 <srdjan.popovic14@gmail.com>
2020-05-27 13:33:59 +0200

    Initial commit

---
+++ src/test/java/com/ftn/isa/IsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftn.isa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
