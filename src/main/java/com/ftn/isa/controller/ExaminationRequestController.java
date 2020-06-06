54266dbf36469893b7c8522081b8d6c4fa67e84d
srdjan14 <srdjan.popovic14@gmail.com>
2020-06-06 04:09:22 +0200

    Dodat kontroler za ocenjivanje doktora/klinika

--- src/main/java/com/ftn/isa/controller/ExaminationRequestController.java
+++ src/main/java/com/ftn/isa/controller/ExaminationRequestController.java
@@ -1,9 +1,33 @@
 package com.ftn.isa.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ftn.isa.dto.response.ExaminationRequestResponse;
+import com.ftn.isa.service.IExaminationRequestService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/examination-request")
 public class ExaminationRequestController {
+
+    private final IExaminationRequestService _examinationRequestService;
+
+    public ExaminationRequestController(IExaminationRequestService examinationRequestService) {
+        _examinationRequestService = examinationRequestService;
+    }
+
+    @PostMapping("/predefined-booking")
+    public void bookingExamination(@RequestParam Long patientId, Long clinicId) {
+        _examinationRequestService.bookingPredefinedExamination(patientId, clinicId);
+    }
+
+    @GetMapping
+    public List<ExaminationRequestResponse> getExaminationRequestes() {
+        return _examinationRequestService.getAllExaminationRequest();
+    }
+
+    @GetMapping("/{id}/patient-examination")
+    public List<ExaminationRequestResponse> getPatientesExaminations(@PathVariable Long patientId) {
+        return _examinationRequestService.findAllExaminationOfPatient(patientId);
+    }
 }
--- src/main/java/com/ftn/isa/controller/ReviewController.java
+++ src/main/java/com/ftn/isa/controller/ReviewController.java
@@ -1,9 +1,32 @@
 package com.ftn.isa.controller;
 
+import com.ftn.isa.dto.request.ClinicReviewRequest;
+import com.ftn.isa.dto.request.DoctorReviewRequest;
+import com.ftn.isa.dto.response.ClinicReviewResponse;
+import com.ftn.isa.dto.response.DoctorReviewResponse;
+import com.ftn.isa.service.IReviewService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/review")
 public class ReviewController {
+
+    private final IReviewService _reviewService;
+
+    public ReviewController(IReviewService reviewService) {
+        _reviewService = reviewService;
+    }
+
+    @PostMapping("/doctor-review")
+    public DoctorReviewResponse reviewDoctor(@RequestBody DoctorReviewRequest request) {
+        return _reviewService.reviewingDoctor(request);
+    }
+
+    @PostMapping("/clinic-review")
+    public ClinicReviewResponse reviewDoctor(@RequestBody ClinicReviewRequest request) {
+        return _reviewService.reviewingClinic(request);
+    }
 }
